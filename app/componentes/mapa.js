83f8e0276f7774c5afec4ce67f4cf592034c7247
juangampr4 <93872278+juangampr4@users.noreply.github.com>
2025-07-11 09:14:59 -0500

    Update mapa.js

--- app/componentes/mapa.js
+++ app/componentes/mapa.js
@@ -1,6 +1,11 @@
 "use client"
 import {MapContainer, TileLayer, Popup, Marker, LayersControl, LayerGroup} from 'react-leaflet'
 import MarkerClusterGroup from "react-leaflet-markercluster";
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import 'leaflet.markercluster/dist/MarkerCluster.css';
+import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
+import './styles.css';
 import "./styles.css";
 import { useState,useEffect } from "react";
 import 'leaflet/dist/leaflet.css';
