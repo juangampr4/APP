58a1c5cf0b941b9e7d9701d7c917b28bebd2ca88
juangampr4 <93872278+juangampr4@users.noreply.github.com>
2025-07-11 09:05:14 -0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 "use client"
 import Navbar from "./componentes/navbar";
-
+import './styles.css';
 import dynamic from "next/dynamic";
 export default function Home() {
   const Mapa = dynamic(() => import ("./componentes/mapa"), {ssr: false})
