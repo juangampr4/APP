2f344d821626687cef1cc3ee56a722f2a87f075a
juangampr4 <93872278+juangampr4@users.noreply.github.com>
2025-07-11 09:06:27 -0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 "use client"
 import Navbar from "./componentes/navbar";
-import './styles.css';
+import 'leaflet.markercluster/dist/MarkerCluster.css';
+import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
 import dynamic from "next/dynamic";
 export default function Home() {
   const Mapa = dynamic(() => import ("./componentes/mapa"), {ssr: false})
